104d1b926eaedf094f1a60e9e090211363a5df9f
nikolay <nikolay-kmc@mail.ru>
2025-03-21 20:52:23 +0300

    newjunit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,33 +1,38 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
-    public void shouldAmountEquallyReturned() {
 
-        int expected = 1000;
-        int actual = service.remain(1000);
-        assertEquals(actual, expected);
+    @org.junit.Test
+    public void testRemainBoundaryValuesTwo() {
+        org.junit.Assert.assertEquals(1000, service.remain(0));
     }
 
-    @Test
-    public void shouldAmountLessReturned() {
-
-        int expected = 1000;
-        int actual = service.remain(0);
-        assertEquals(actual, expected);
+    @org.junit.Test
+    public void testRemainBoundaryValuesThree() {
+        org.junit.Assert.assertEquals(999, service.remain(1));
     }
 
-    @Test
-    public void shouldAmountMoreReturned() {
+    @org.junit.Test
+    public void testRemainBoundaryValuesFour() {
+        org.junit.Assert.assertEquals(1, service.remain(999));
+    }
 
-        int expected = 0;
-        int actual = service.remain(1000);
-        assertEquals(actual, expected);
+    @org.junit.Test
+    public void testRemainBoundaryValuesFive() {
+        org.junit.Assert.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.Test
+    public void testRemainBoundaryValuesSix() {
+        org.junit.Assert.assertEquals(999, service.remain(1001));
+    }
+
+    @org.junit.Test
+    public void testRemainCriticalPath() {
+        org.junit.Assert.assertEquals(600, service.remain(400));
     }
 }
